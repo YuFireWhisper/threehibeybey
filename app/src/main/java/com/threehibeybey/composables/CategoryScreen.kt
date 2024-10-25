d53c1b07b0b917d90ae4e05e5a8e34ae28cabc0f
YuFireWhisper <yu.whisper.personal@gmail.com>
2024-10-25 14:46:18 +0800

    fix: `CategoryScreen.kt`

--- app/src/main/java/com/threehibeybey/composables/CategoryScreen.kt
+++ app/src/main/java/com/threehibeybey/composables/CategoryScreen.kt
@@ -3,6 +3,7 @@ package com.threehibeybey.composables
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
